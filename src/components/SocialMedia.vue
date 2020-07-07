86ec173130afb4278bb189ee463d33b196968a49
kostiantynkushch <kostiantyn.kushch@gmail.com>
2020-07-07 21:50:38 +0300

    linked svg sprite

--- src/components/SocialMedia.vue
+++ src/components/SocialMedia.vue
@@ -1,12 +1,8 @@
 <template>
   <div class="ba-social">
-    <svg class="icon">
-      <use xlink:href="@/assets/icons/sprite.svg#instagram" />
-    </svg>
     <a v-for="item in socialButtons" :key="item.key" :href="item.link">
-      {{item.name}}
       <svg class="icon">
-        <use :xlink:href="item.icon" />
+        <use :xlink:href="require('@/assets/icons/sprite.svg')+item.icon" />
       </svg>
     </a>
   </div>
@@ -22,37 +18,37 @@ export default {
           key: 1,
           name: "call",
           link: "#",
-          icon: "@/assets/icons/sprite.svg#call"
+          icon: "#call"
         },
         {
           key: 2,
           name: "instagram",
           link: "#",
-          icon: "@/assets/icons/sprite.svg#instagram"
+          icon: "#instagram"
         },
         {
           key: 3,
           name: "vk",
           link: "#",
-          icon: "@/assets/icons/sprite.svg#vk"
+          icon: "#vk"
         },
         {
           key: 4,
           name: "twitter",
           link: "#",
-          icon: "@/assets/icons/sprite.svg#twitter"
+          icon: "#twitter"
         },
         {
           key: 5,
           name: "facebook",
           link: "#",
-          icon: "@/assets/icons/sprite.svg#facebook"
+          icon: "#facebook"
         },
         {
           key: 6,
           name: "ok",
           link: "#",
-          icon: "@/assets/icons/sprite.svg#ok"
+          icon: "#ok"
         }
       ]
     };
@@ -61,4 +57,8 @@ export default {
 </script>
 
 <style>
+.icon {
+  width: 40px;
+  height: 40px;
+}
 </style>
