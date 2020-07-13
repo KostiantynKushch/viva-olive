ed345ffa8b5029edc3edf21d63d762a3b40719d9
andrii-herasimenko <andriigerasimenko@gmail.com>
2020-07-13 18:52:43 +0300

    season section, booking section, little fixes

--- src/views/sections/Booking.vue
+++ src/views/sections/Booking.vue
@@ -2,7 +2,8 @@
   <section class="ba-booking">
     <div class="ba-container">
       <div class="ba-booking__inner">
-        <h5>Booking section goes here</h5>
+        <h5 class="ba-booking__inner-title">Забронювати столик</h5>
+		  <img class="ba-booking__inner-img" src="../../assets/img/booking-section-img.png" alt="">
       </div>
       <!-- /.ba-booking__inner -->
     </div>
@@ -15,5 +16,62 @@
 export default {};
 </script>
 
-<style>
+<style lang="scss">
+.ba-booking{
+			@media (max-width:767px) {
+				display:none;
+			}
+			
+			&__inner{
+				
+				&-title{
+					display: block;
+					margin: 0 auto;
+					text-align: center;
+					position: relative;
+					
+					color: #fff;
+					font-size: 30px;
+					@media (min-width: 768px) {
+						font-size: 25px;
+						padding: 150px 0;
+					}
+					@media (min-width: 1024px) {
+						font-size: 25px;
+						padding: 150px 0;
+					}
+					@media (min-width: 1440px) {
+						font-size: 30px;
+						padding: 150px 0;
+					}
+					
+					&:before{
+						content: '';
+						position: absolute;
+						background-image: url("../../assets/img/section_bg_title_left.png") ;
+						background-size: cover;
+						z-index:-1;
+						@media (min-width: 768px) {
+						top:30%;
+						left:10%;
+						width: 520px;
+						height: 157px;
+						}
+						@media (min-width: 900px) {
+						left:25%;
+						}
+						@media (min-width: 1024px) {
+						left:25%;
+						}
+						@media (min-width: 1440px) {
+						left:30%;
+						}
+					}
+}
+			}
+
+}
+
+
+
 </style>
--- src/views/sections/Season.vue
+++ src/views/sections/Season.vue
@@ -2,7 +2,13 @@
   <section class="ba-season">
     <div class="ba-container">
       <div class="ba-season__inner">
-        <h5>Season section goes here</h5>
+        <a class="ba-season__inner--btn" @click="onMenu">
+			  <span class="ba-season__inner--btn-text">Сезонне меню</span>
+			  <span class="ba-season__inner--btn-icon">
+				  <img src="../../assets/img/down-arrow.png" alt="">
+			  </span>
+		  </a>
+		  <img v-show="isVisibleMenu" class="ba-season__inner--img" src="../../assets/img/season-menu.jpg" alt="">
       </div>
       <!-- /.ba-season__inner -->
     </div>
@@ -12,8 +18,49 @@
 </template>
 
 <script>
-export default {};
+export default {
+	name: "index",
+	data: function () {
+    return {
+      isVisibleMenu: false
+    }
+  },
+	methods:{
+		onMenu(){
+			this.isVisibleMenu = !this.isVisibleMenu;
+		}
+	}
+};
 </script>
 
-<style>
+<style lang="scss">
+
+.ba-season__inner{
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	&--btn{
+		margin: 50px 0;
+		padding: 10px;
+		border: 2px solid #747436;
+		border-radius: 5px;
+		text-decoration: none;
+		text-transform: uppercase;
+		background: #C4CCA8;
+		color: #747436;
+		font-family: Montserrat;
+		font-style: normal;
+		font-weight: 600;
+		font-size: 14px;
+		line-height: 27px;
+		max-width: 180px;
+		&-icon{
+			padding-left: 10px;
+		}
+		&:hover{
+			background: #fff;
+		}
+	}
+}
+
 </style>
